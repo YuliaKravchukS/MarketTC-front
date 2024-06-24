113b69bd36768b5f3bca21454bc48b72bb30e827
Yullia Kravchuk <ljuljek554@gmail.com>
2024-06-24 15:59:53 +0300

    add styles

--- src/components/CatalogAllProducts.tsx
+++ src/components/CatalogAllProducts.tsx
@@ -1,19 +1,17 @@
-import { catalogProducts } from '@/db/catalogProducts';
-import CategoryItem from './CategoryItem';
-import { Category, DataProducts } from './types';
+import { catalogProducts } from "@/db/catalogProducts";
+import CategoryItem from "./CategoryItem";
+import { Category, DataProducts } from "./types";
 
 const dataProducts: DataProducts = JSON.parse(JSON.stringify(catalogProducts));
 
 const data: Category[] = dataProducts.categories;
-console.log('data: ', data);
+console.log("data: ", data);
 
 export default function CatalogAllProducts() {
   return (
-
-    <div className='container flex flex-col justify-center items-center gap-[26px]  tablet: gap-[32px] w-[576px] laptop: gap-[36px]'>
+    <div className='m-auto flex flex-col justify-center items-center gap-[26px] md:gap-[32px] md:w-[576px] lg:gap-[36px] lg:w-[1192px]'>
       <h2>Catalog</h2>
-      <ul className='w-full grid grid-cols-1 gap-6  tablet:grid-cols-2 gap-x-[30px] gap-y-12 laptop:grid-cols-4 gap-y-9'>
-
+      <ul className='w-full grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-x-[30px] md:gap-y-12 lg:grid-cols-4 lg:gap-y-10'>
         {data.map((product: Category) => (
           <li className='flex gap-5 w-full h-[310px]' key={product.id}>
             <CategoryItem product={product} />
