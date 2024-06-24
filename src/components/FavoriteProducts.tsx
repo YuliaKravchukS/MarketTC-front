8d1dae3340e1af1b77c66b2dec0fd5d6370b15bd
petelkavictor <p.v.victor322@gmail.com>
2024-06-24 20:14:16 +0300

    Update styles for carousel and update tailwind.config.ts screen width variable

--- src/components/FavoriteProducts.tsx
+++ src/components/FavoriteProducts.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ProductCard from './ProductCard';
 import ProductCarousel from './ProductCarousel';
 
 const FavoriteProducts = () => {
   return (
-    <section className=" pt-9 pb-7">
+    <section className="  pt-9 pb-7 px-0.5">
       <h6 className="text-center">Favorite</h6>
-      <div className="mt-9  flex gap-1 desktop:gap-3 laptop:gap-2 tablet:gap-1  justify-center">
+      <div className="mt-9 flex   justify-center">
         <ProductCarousel />
       </div>
     </section>
--- src/components/ProductCarousel.tsx
+++ src/components/ProductCarousel.tsx
@@ -17,15 +17,15 @@ const ProductCarousel = () => {
         loop: true,
       }}
       className="w-full max-w-[1188px] mx-auto ">
-      <CarouselContent className="  -ml-1 laptop:-ml-3 desktop:-ml-3 pb-2 ">
+      <CarouselContent className="-ml-1  2xl:-ml-3 pb-2 ">
         {Array.from({ length: 9 }).map((_, index) => (
-          <CarouselItem key={index} className="basis-auto pl-1 laptop:pl-3 desktop:pl-3 shrink-0">
+          <CarouselItem key={index} className="basis-auto pl-1 2xl:pl-3 shrink-0   ">
             <ProductCard />
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="carousel-arrow carousel-arrow-prev hidden tablet:inline-flex laptop:inline-flex desktop:inline-flex left-0 tablet:left-4 laptop:left-2 desktop:left-1 " />
-      <CarouselNext className="carousel-arrow carousel-arrow-next hidden tablet:inline-flex  laptop:inline-flex desktop:inline-flex right-0 tablet:right-4 laptop:right-2 desktop:right-1" />
+      <CarouselPrevious className="carousel-arrow carousel-arrow-prev hidden md:inline-flex  left-4  2xl:left-1 " />
+      <CarouselNext className="carousel-arrow carousel-arrow-next hidden md:inline-flex  right-4   2xl:right-1" />
     </Carousel>
   );
 };
