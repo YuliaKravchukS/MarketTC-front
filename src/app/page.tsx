ed2be3efd7b78c5c65f3c1b107bae35cc575b0ba
petelkavictor <p.v.victor322@gmail.com>
2024-06-27 08:48:32 +0300

    create hero-section, update tailwind.config.ts, global.css and fetch for health-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import CatalogAllProducts from '@/components/CatalogAllProducts';
 import FavoriteProducts from '@/components/FavoriteProducts';
+import HeroSection from '@/components/HeroSection';
 import PromotionalProducts from '@/components/PromotionalProducts';
 
 export default function Home() {
   return (
-    <main className=" max-w-screen-2xl  h-full xl:px-20 2xl:px-[115px]    mx-auto  ">
+    <main className=" max-w-screen-2xl  h-full xl:px-20 2xl:px-[115px]  md:mt-[31px] xl:pt-[32px]  mx-auto  xl:pb-[50px] 2xl:pb-0 ">
+      <HeroSection />
       <PromotionalProducts />
       <CatalogAllProducts />
       <FavoriteProducts />
