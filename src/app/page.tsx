1e023b9c3c7560b81b38622886e224207c74159c
petelkavictor <p.v.victor322@gmail.com>
2024-08-02 15:26:46 +0300

    commented search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
-import CatalogAllProducts from "@/components/CatalogAllProducts";
-import FavoriteProducts from "@/components/FavoriteProducts";
-import HeroSection from "@/components/HeroSection";
-import PromotionalProducts from "@/components/PromotionalProducts";
-import { Suspense } from "react";
-import Loading from "./loading";
+import CatalogAllProducts from '@/components/CatalogAllProducts';
+import FavoriteProducts from '@/components/FavoriteProducts';
+import HeroSection from '@/components/HeroSection';
+import PromotionalProducts from '@/components/PromotionalProducts';
+import { Suspense } from 'react';
+import Loading from './loading';
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams?: { query: string };
-}) {
-  const query = searchParams?.query || "";
+export default function Home() {
+//   {
+//   searchParams,
+// }: {
+//   searchParams?: { query: string };
+// }
+  // const query = searchParams?.query || "";
 
   return (
-    <main className=' max-w-screen-2xl  h-full xl:px-20 2xl:px-[115px]  md:mt-[31px] xl:pt-[32px]  mx-auto  xl:pb-[50px] 2xl:pb-0 '>
+    <main className=" max-w-screen-2xl  h-full xl:px-20 2xl:px-[115px]  md:mt-[31px] xl:pt-[32px]  mx-auto  xl:pb-[50px] 2xl:pb-0 ">
       <HeroSection />
       <Suspense fallback={<Loading />}>
         <PromotionalProducts />
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,25 +1,24 @@
-import Image from "next/image";
-import React from "react";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
-import Search from "./Search";
+import Image from 'next/image';
+import React from 'react';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+import Search from './Search';
 
 const HeroSection = () => {
   return (
-    <section className=' pt-8 xl:max-w-[1037px] mx-auto 2xl:max-w-[1198px] mb-[37px] md:mb-9 xl:mb-3 2xl:mb-[42px]'>
-      <div className='flex flex-wrap mb-2 md:flex-nowrap mx-[23px]  xl:mx-0 justify-center pb-8 md:gap-5 xl:gap-[60px] 2xl:gap-[100px] '>
-        <Button className='order-2 border text-background-100  max-h-[50px] font-semibold  border-primary-500  bg-primary-500 hover:bg-secondary-500 hover:border-secondary-500 active:bg-white active:text-secondary-500 h-full max-w-[234px] w-full px-6 py-[11.5px] leading-[27px] text-[18px]'>
+    <section className=" pt-8 xl:max-w-[1037px] mx-auto 2xl:max-w-[1198px] mb-[37px] md:mb-9 xl:mb-3 2xl:mb-[42px]">
+      <div className="flex flex-wrap mb-2 md:flex-nowrap mx-[23px]  xl:mx-0 justify-center pb-8 md:gap-5 xl:gap-[60px] 2xl:gap-[100px] ">
+        <Button className="order-2 border text-background-100  max-h-[50px] font-semibold  border-primary-500  bg-primary-500 hover:bg-secondary-500 hover:border-secondary-500 active:bg-white active:text-secondary-500 h-full max-w-[234px] w-full px-6 py-[11.5px] leading-[27px] text-[18px]">
           Catalog
         </Button>
         <Search />
       </div>
       <Image
-        className='  block w-full h-[143px] md:h-[184px] xl:h-[188px]  '
-        src='/images/rectangle.jpg'
-        alt=''
+        className="  block w-full h-[143px] md:h-[184px] xl:h-[188px]  "
+        src="/images/rectangle.jpg"
+        alt=""
         width={375}
-        height={143}
-      ></Image>
+        height={143}></Image>
     </section>
   );
 };
