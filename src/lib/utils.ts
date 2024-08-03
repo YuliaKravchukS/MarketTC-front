704e6566bdc8463e11931c4bc606a0945a7e8f85
petelkavictor <p.v.victor322@gmail.com>
2024-08-03 18:30:52 +0300

    remove getData() from utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,6 @@
-import { API_URL } from '@/constants';
-import axios from 'axios';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export async function getData<T>(link: string): Promise<T> {
-  try {
-    const data = (await axios.get(`${API_URL}${link}`)).data;
-    return data;
-  } catch (error) {
-    console.error('Failed to fetch data:', error);
-    throw error;
-  }
 }
